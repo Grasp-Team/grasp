3c82d4b8912db20ebb89e5c53ceff8babaa29e8c
Jacob Moore <jacobaemoore@gmail.com>
2017-10-14 18:56:05 -0400

    added scripts

---
+++ scripts/schema.sql
@@ -0,0 +1,26 @@
+﻿
+CREATE SCHEMA course;
+
+CREATE TABLE course.courseCatalog (
+	courseName text PRIMARY KEY
+	-- fill in other information
+);
+
+CREATE SCHEMA users;
+
+CREATE TABLE users.users (
+	id uuid PRIMARY KEY,
+	firstName text NOT NULL,
+	lastName text NOT NULL,
+	email text NOT NULL UNIQUE,
+	year integer NOT NULL,
+	program text,
+	faculty text
+);
+
+CREATE TABLE users.roles (
+	id uuid PRIMARY KEY,
+	course text REFERENCES course.courseCatalog (courseName)
+	--other information
+);
+
