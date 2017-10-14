e275f5c76bd7616a748fb34d0552435a12e609a5
Jacob Moore <jacobaemoore@gmail.com>
2017-10-14 19:33:37 -0400

    f

---
+++ src/main/java/com/grasp/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.grasp.dao;
+
+import com.grasp.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
+import java.util.UUID;
+
+@Transactional
+public interface UserDao extends CrudRepository<User, UUID>{
+    User findUserById(UUID id);
+}
--- src/main/java/com/grasp/model/User.java
+++ src/main/java/com/grasp/model/User.java
@@ -22,15 +22,67 @@ public class User {
     private UUID id;
     @Column(name = "firstName", nullable = false)
     private String firstName;
-    @Column(name = "firstName", nullable = false)
+    @Column(name = "lastName", nullable = false)
     private String lastName;
-    @Column(name = "firstName", nullable = false)
+    @Column(name = "email", nullable = false)
     private String email;
-    @Column(name = "firstName", nullable = false)
+    @Column(name = "year", nullable = false)
     private int year;
-    @Column(name = "firstName", nullable = false)
+    @Column(name = "program")
     private String program;
-    @Column(name = "firstName", nullable = false)
+    @Column(name = "faculty")
     private String faculty;
 
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, int year, String program, String faculty) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.year = year;
+        this.program = program;
+        this.faculty = faculty;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (year != user.year) return false;
+        if (!id.equals(user.id)) return false;
+        if (!firstName.equals(user.firstName)) return false;
+        if (!lastName.equals(user.lastName)) return false;
+        if (!email.equals(user.email)) return false;
+        if (program != null ? !program.equals(user.program) : user.program != null) return false;
+        return faculty != null ? faculty.equals(user.faculty) : user.faculty == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + year;
+        result = 31 * result + (program != null ? program.hashCode() : 0);
+        result = 31 * result + (faculty != null ? faculty.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", year=" + year +
+                ", program='" + program + '\'' +
+                ", faculty='" + faculty + '\'' +
+                '}';
+    }
 }
