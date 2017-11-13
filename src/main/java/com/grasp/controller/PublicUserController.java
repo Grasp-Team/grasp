7199a6c98692142cb5efb679f9e251555b7efd2d
Jacob Moore <jacobaemoore@gmail.com>
2017-11-13 18:06:08 -0500

    added antmatcher properly

--- src/main/java/com/grasp/controller/PublicUserController.java
+++ src/main/java/com/grasp/controller/PublicUserController.java
@@ -1,10 +1,7 @@
 package com.grasp.controller;
 
-import com.grasp.exception.ControllerException;
 import com.grasp.model.dto.InUseDTO;
 import com.grasp.model.entity.User;
-import com.grasp.model.dto.UserDTO;
-import com.grasp.model.util.EntityConverter;
 import com.grasp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,23 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublicUserController {
 
     private UserService userService;
-    private EntityConverter entityConverter;
 
     @Autowired
-    public PublicUserController(UserService userService, EntityConverter entityConverter) {
+    public PublicUserController(UserService userService) {
         this.userService = userService;
-        this.entityConverter = entityConverter;
     }
 
 
     @RequestMapping(value = "/email/{email}", method = RequestMethod.GET)
     public ResponseEntity<InUseDTO> getByName(@PathVariable("email") String email) {
+        System.out.println("EMAIL: " + email);
         User user = userService.getByEmail(email);
 
         if (user == null) {
-            return new ResponseEntity<>(new InUseDTO(false, "Email" + email + " not in use."), HttpStatus.OK);
+            return new ResponseEntity<>(new InUseDTO(false, "Email " + email + " not in use."), HttpStatus.OK);
         }
 
-        return new ResponseEntity<>(new InUseDTO(true, "Email" + email + " in use."), HttpStatus.OK);
+        return new ResponseEntity<>(new InUseDTO(true, "Email " + email + " in use."), HttpStatus.OK);
     }
 }
